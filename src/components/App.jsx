ad8fad38b8ba33bd6f10c34a4f9daaa05a161d76
JavierLlanos6 <xavi6poop@gmail.com>
2022-11-02 23:43:04 -0400

    architectured finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './App.module.css';
+import { Class } from './Class/Class';
 
 export const App = () => {
   return (
     <div className={styles.App}>
       <div className={styles.Header}>
-        Header
+      <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-1-circle" viewBox="0 0 16 16">
+  <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
+</svg>
+<div>Edukit</div>
+<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-divst" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
+<div className={styles.User}>
+  User
+</div>
       </div>
       <div className={styles.test}>
         <div className={styles.NavBar}>
-          NavBar
+          <div className={styles.Items}>
+            <div>  <p>Clases</p></div>
+            <div>  <p>Material</p></div>
+            <div>  <p>Chat</p></div>
+            <div> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-divst" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg> Documentos</div>
+          </div>
+          
         </div>
         <div className={styles.Content}>
-          Content
+          Mis Clases
+          <Class />
         </div>
       </div>
     </div>
---
+++ src/components/Class/Class.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Class.module.css'
+
+export const Class = () => {
+  return (
+    <div className={styles.Class}>
+        <p>Clase 1-2022</p>
+        <div className={styles.ClassImage}>
+
+        </div>
+    </div>
+  )
+}
+
